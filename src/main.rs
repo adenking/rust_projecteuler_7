f04b87daa081f469e06feefb7ff50ede0a3bcba4
Aden King <adenking@live.com>
2020-02-13 10:38:04 +1000

    recreated the project euler 7 in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate integer_sqrt;
+// `use` trait to get functionality
+use integer_sqrt::IntegerSquareRoot;
+
 fn main() {
-    println!("Hello, world!");
+    let mut prime_numbers = Vec::new();
+    prime_numbers.push(2);
+    for num in 3..200001 {
+        if prime_numbers.len() == 10002{
+            break;
+        }
+        let mut prime = true;
+        for num_check in 2..num.integer_sqrt() {
+            if num % num_check == 0 {
+                prime = false;
+                break;
+            }
+        }
+        match prime {
+            true => prime_numbers.push(num),
+            false => continue,
+        }
+    }
+    println!("{}", prime_numbers[10001]);
 }
